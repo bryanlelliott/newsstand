9430d116e03d80bb00471a34f8f6286edbf30c23
francis <francis024780@gmail.com>
2015-12-13 20:36:11 -0500

    Added form and action to struts.xml

---
+++ src/java/model/RatingList.java
@@ -0,0 +1,28 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package model;
+
+/**
+ *
+ * @author francis
+ */
+public class RatingList {
+    String[] array = {
+        "1", 
+        "2", 
+        "3",
+        "4",
+        "5"
+    };
+
+    public void setArray(String[] array) {
+        this.array = array;
+    }
+
+    public String[] getArray() {
+        return array;
+    }
+}
